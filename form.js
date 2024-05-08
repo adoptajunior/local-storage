3c762ee455f46b0b82139ec79130fa1856e90637
JON <jdlt@duck.com>
2024-05-08 20:50:57 +0200

    ejercicio local storage

---
+++ form.js
@@ -0,0 +1,27 @@
+localStorage.setItem('user', JSON.stringify({
+
+}
+))
+
+const yourName = document.getElementById('name');
+const yourEmail = document.getElementById('email');
+const yourText = document.getElementById('text');
+const submitbtn = document.getElementById('submit');
+
+submitbtn.addEventListener('click', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log('hola');
+
+
+    const nameValue = yourName.value;
+    const emailValue = yourEmail.value;
+    const textValue = yourText.value;
+
+    // console.log(firstNameValue);
+
+    localStorage.setItem('name', nameValue);
+    localStorage.setItem('email', emailValue);
+    localStorage.setItem('text', textValue);
+}
